7b4cfc8afa2104b09c48fb070910ae0878632ba0
infected_ <infectedmushi@gmail.com>
2018-10-18 00:12:05 +0100

    build with GCC 7.3.x
    Change-Id: Ide5960f0c6d863045295d10c73bde745645f4ac3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,7 +74,8 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-gnu-7.3/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
